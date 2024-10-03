a31313ea4054515143de90109e14a6edb5d7815a
Raihan Adi <122330519+hansrehan12@users.noreply.github.com>
2024-10-03 19:36:27 +0700

    Dictionary
    Tipe data Dictionary digunakan untuk mengelola data besar dan akses anggota secara acak. Tidak memiliki indeks, dan dideklarasikan dengan kurung kurawal { } dalam format kunci : nilai (key-value), dipisahkan dengan koma. Anggota dapat diakses dengan fungsi get(), yang mengembalikan None jika kunci tidak ada. Tanpa get(), akan terjadi KeyError jika kunci tidak ditemukan.

---
+++ Praktikum26.py
@@ -0,0 +1,20 @@
+# Raihan Adi Wijiyanto
+# 2411501550
+# Praktikum 26
+# Tipe data Dictionary
+
+student = {
+    "nama" : "Faqih",
+    "umur" : 26,
+    "tinggi" : 177.6,
+    "hobi" : ["Olahraga" , "Jalan-Jalan"],
+    "kontak" : {
+        "website" : "faqihkeren.com",
+        "email" : "faqihkeren.com"
+    }  
+}
+print("Nama : ", student.get('nama') )
+print("Umur :" , student.get('umur'))
+print("Tinggi : " , student.get('tinggi'))
+print("Hobi : ", student.get('hobi'))
+print("Kontak : " , student.get('kontak') )
